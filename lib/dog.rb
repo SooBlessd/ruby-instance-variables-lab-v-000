a3321f109290d0a3bfd2cbdcd2fecca7d35367b2
Valencia Caldwell <vkcaldwell@gmail.com>
2021-01-08 02:07:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,13 @@
 #   def name=(dog_name)
 #     @this_dogs_name = dog_name
 #   end
-# 
+#
 #   def name
 #     @this_dogs_name
 #   end
 # end
-# 
+#
 # lassie = Dog.new
 # lassie.name = "Lassie"
-# 
+#
 # lassie.name
